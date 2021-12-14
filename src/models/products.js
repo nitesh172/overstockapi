02305c4e1d784ac27d39be4d9647d8ac3763eb01
nitesh172 <niteshbaghel172@gmail.com>
2021-12-14 15:48:46 +0530

    first commit

--- src/models/products.js
+++ src/models/products.js
@@ -2,67 +2,64 @@ const mongoose = require("mongoose");
 const validator = require("validator");
 
 const productSchema = new mongoose.Schema({
-    // imageURL: {
-    //     type: String,
-    //     required: true,
-    //     minlength: 20
-    // },
-    // img1: {
-    //     type: String,
-    //     required: true
-    // },
-    // img2: {
-    //     type: String,
-    //     required: true
-    // },
-    // img3: {
-    //     type: String,
-    //     required: true
-    // },
-    // img4: {
-    //     type: String,
-    //     required: true
-    // },
-    // name: {
-    //     type: String,
-    //     required: true,
-    //     minlength: 10
-    // },
-    // desc: {
-    //     type: String,
-    //     required: true
-    // },
-    // oldPrice: {
-    //     type: String,
-    //     required: true
-    // },
-    // newPrice: {
-    //     type: String,
-    //     required: true
-    // },
-    // off: {
-    //     type: String,
-    //     required: true
-    // },
-    // category: {
-    //     type: String,
-    //     required: true
-    // },
-    // color: {
-    //     type: String,
-    //     required: true
-    // },
-    // size: {
-    //     type: String,
-    //     required: true
-    // },
-
     imageURL: {
         type: String,
         required: true,
-        minlength: 20
     },
-    
+    img1: {
+        type: String,
+        required: true
+    },
+    img2: {
+        type: String,
+        required: true
+    },
+    img3: {
+        type: String,
+        required: true
+    },
+    img4: {
+        type: String,
+        required: true
+    },
+    img5: {
+        type: String,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true,
+        minlength: 10
+    },
+    rating: {
+        type: Number,
+        required: true
+    },
+    color: {
+        type: Array,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    main_catergory: {
+        type: String,
+        required: true
+    },
+    main_sub_catergory: {
+        type: String,
+        required: true
+    },
+    sub_catergory: {
+        type: String,
+        required: true
+    },
+    catergory: {
+        type: String,
+        required: true
+    },
+
 })
 
 // we will create a new collection 
