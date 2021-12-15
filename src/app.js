d2654b49108635c0dc9a0dc803f03da3f893a29d
nitesh172 <niteshbaghel172@gmail.com>
2021-12-15 17:53:23 +0530

    Update by popular

--- src/app.js
+++ src/app.js
@@ -1,41 +1,68 @@
 const express = require("express");
 require("./db/conn");
-const Product = require("./models/products")
+const Product = require("./models/products");
+const Popular = require("./models/popular");
 const app = express();
-const port = process.env.PORT || 3000; 
+const port = process.env.PORT || 3000;
 
-var cors = require('cors')
- 
-app.use(cors())
+var cors = require("cors");
+
+app.use(cors());
 
 // create a new product
 
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.send("hello from the other sideby Nitesh");
+  res.send("hello from the other sideby Nitesh");
 });
 
 app.post("/products", (req, res) => {
-    console.log(req.body);
-    const pro = new Product(req.body);
-    pro.save().then(() => {
-        res.status(201).send(pro);
-    }).catch((e) => {
-        res.status(201).send(e);
+  console.log(req.body);
+  const pro = new Product(req.body);
+  pro
+    .save()
+    .then(() => {
+      res.status(201).send(pro);
     })
-})
+    .catch((e) => {
+      res.status(201).send(e);
+    });
+});
+
+app.post("/popular", (req, res) => {
+  console.log(req.body);
+  const pop = new Popular(req.body);
+  pop
+    .save()
+    .then(() => {
+      res.status(201).send(pop);
+    })
+    .catch((e) => {
+      res.status(201).send(e);
+    });
+});
 
 app.get("/products", async (req, res) => {
-    try {
-        const productsData = await Product.find();
-        console.log(productsData);
-        res.send(productsData);
-    } catch (error) {
-        res.send(error);
-    }
-}) 
+  try {
+    const productsData = await Product.find();
+    console.log(productsData);
+    res.send(productsData);
+  } catch (error) {
+    res.send(error);
+  }
+});
+
+app.get("/popular", async (req, res) => {
+  try {
+    const popularData = await Product.find();
+    console.log(popularData);
+    res.send(popularData);
+  } catch (error) {
+    res.send(error);
+  }
+});
 
 app.listen(port, () => {
-    console.log(`connection is setup at ${port}`);
+  console.log(`connection is setup at ${port}`);
 });
